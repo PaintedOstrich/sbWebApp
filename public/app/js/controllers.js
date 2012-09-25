7af341eab4eccdc698df1001975af71939497582
Codier <pendyaaa@gmail.com>
2012-09-25 12:02:13 -0500

    remove dead code

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -2,20 +2,6 @@
 
 /* Controllers */
 
-// Temporarily leave here as an example
-// function PhoneDetailCtrl($scope, $routeParams, Phone) {
-//   $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-//     $scope.mainImageUrl = 'app/' + phone.images[0];
-//   });
-//
-//   $scope.setImage = function(imageUrl) {
-//     $scope.mainImageUrl = imageUrl;
-//   }
-// }
-
-//PhoneDetailCtrl.$inject = ['$scope', '$routeParams', 'Phone'];
-
-
 // The very first controller to be reached that routes traffic to
 // landing page for visitors and profile page for returning user.
 function RouteCtrl($scope, $location, fb) {
