306273cb6aae225114f184de0e596966b619d23a
Codier <pendyaaa@gmail.com>
2012-09-25 09:56:40 -0500

    fix up e2e test

--- public/test/e2e/scenarios.js
+++ public/test/e2e/scenarios.js
@@ -3,20 +3,9 @@
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
 describe('Sports Bet App', function() {
-
-  it('should redirect to index.html#/home', function() {
-    browser().navigateTo('../../');
-    expect(browser().location().url()).toBe('/home');
-  });
-  
-  it('should highlight appropriate nav bar tab', function() {
-    browser().navigateTo('../../#/bet');
-    expect(browser().location().url()).toBe('/bet');
-    expect(element('#mainNav li.home.active').count()).toEqual(0);
-    expect(element('#mainNav li.bet.active').count()).toEqual(1);
-    
-    expect(element('#mainNav li.profile.active').count()).toEqual(0);
-    element('#mainNav li.profile a').click();
-    expect(element('#mainNav li.profile.active').count()).toEqual(1);
+  it('should render call for action button', function() {
+    browser().navigateTo('../../#/login');
+    expect(browser().location().url()).toBe('/login');
+    expect(element('.callForActionPanel').count()).toEqual(1);
   });
 });
