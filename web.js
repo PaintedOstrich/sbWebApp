e1826d86aa59a887e9cf4af4df0c787cb8064909
Codier <pendyaaa@gmail.com>
2012-10-07 00:09:43 -0500

    Compile style sheets from less to css

--- web.js
+++ web.js
@@ -5,13 +5,37 @@ var http = require('http');
 var pageLocals = require('./middleware/pageLocals');
 var bodyLimiter = require('./middleware/bodyLimiter');
 var routes = require('./routes/index');
+var lessMiddleware = require('less-middleware');
 
 // create an express webserver
 var app = express();
 
+var environment = process.env.environment || 'development';
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.use(express.favicon(__dirname + '/public/app/img/favicon.ico'));
+
+  // development only
+  if (environment == 'development') {
+    console.log('In Development Env');
+    app.use(lessMiddleware({
+        src: __dirname + '/public',
+        debug: true,
+        force: true
+    }));
+  }
+
+  // production only
+  if (environment == 'production') {
+    console.log('In Production Env');
+    app.use(lessMiddleware({
+        src: __dirname + '/public',
+        compress: true,
+        once: true
+    }));
+  }
+
   app.use(express.static(__dirname + '/public'));
   app.use(bodyLimiter);
   app.use(express.bodyParser());
@@ -23,22 +47,19 @@ app.configure(function(){
     secret: process.env.FACEBOOK_SECRET,
     scope:  'user_likes,user_photos,user_photo_video_tags'
   }));
+
+  // development only
+  if (environment == 'development') {
+    app.use(express.logger('dev'));
+    app.use(express.errorHandler());
+  }
+
+  // production only
+  if (environment == 'production') {
+    app.use(express.logger());
+  }
 });
 
-
-var environment = process.env.environment || 'development';
-// development only
-if (environment == 'development') {
-  console.log('In Development mode!');
-  app.use(express.logger('dev'));
-  app.use(express.errorHandler());
-}
-
-// production only
-if (environment == 'production') {
-  console.log('In Production mode!');
-  app.use(express.logger());
-}
 
 var server = http.createServer(app);
 server.listen(app.get('port'), function(){
